4e38622d0ad294bd021f0fc84755d476c92f10aa
anaskapadia <anas24032003@gmail.com>
2026-08-10 09:56:42 +0530

    Image Size

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -29,6 +29,6 @@ const fileFilter = (req, file, cb) => {
 
 module.exports = multer({
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 },
+    limits: { fileSize: 10 * 1024 * 1024 },
     fileFilter
 });
